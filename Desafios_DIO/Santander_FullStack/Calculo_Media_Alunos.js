3c91728efe3d823a5341ad4bba803e4fe54ea402
zelula1955 <zelula1955@gmail.com>
2021-07-24 02:00:07 -0300

    Alteração no Desafio (Media de Alunos)

--- Desafios_DIO/Santander_FullStack/Calculo_Media_Alunos.js
+++ Desafios_DIO/Santander_FullStack/Calculo_Media_Alunos.js
@@ -5,44 +5,44 @@ import java.util.Scanner;
 public class Desafio
 {
 	public static void main(String[] args)  throws IOException 
-	{
+	{ 
 		Scanner sc = new Scanner(System.in);
 		sc.useLocale(Locale.ENGLISH);
 		Locale.setDefault(new Locale("en", "US"));
 		
-		float n1,n2,n3,n4,Media,EmExame,EmExameFinal;
-		
+		float n1,n2,n3,n4,Media;
+		Double EmExame,EmExameFinal;
 		n1 = sc.nextFloat();
 		n2 = sc.nextFloat();
 		n3 = sc.nextFloat();
 		n4 = sc.nextFloat();
 
 		Media = ((n1*2) + (n2*3) + (n3*4) + (n4*1)) / 10;
-		System.out.printf("Media: %.1f\n",Media);
+		System.out.printf("Media: %.1f%n",Media);
 		//continue a solucao de acordo com o enunciado
 		if ( Media >= 7.0 )
 		{
-			System.out.printf("Aluno aprovado.\n");	
+			System.out.println("Aluno aprovado.");	
 		}
-		else 
-		if ( Media >= 4.9  && Media <= 6.9 )
+		if (media < 5.0) {
+      		System.out.println("Aluno reprovado.");
+    }
+		if ( Media >= 5.0  && Media <= 6.9 )
 		{ 
-		  EmExame = sc.nextFloat();
-			System.out.printf("Aluno em exame.\n");
-			System.out.printf("Nota do exame: %.1f\n", EmExame  );
-		  EmExameFinal = (Media + EmExame) / 2 ;
+		  	EmExame = sc.nextDouble();
+			System.out.println("Aluno em exame.");
+			System.out.printf("Nota do exame: %.1f%n", EmExame  );
+		  	EmExameFinal = (Media + EmExame) / 2.0 ;
 			if( EmExameFinal >= 5.0 )
 			  {
-				  System.out.println("Aluno aprovado.\n");
-		//		System.out.printf("Media final: %.1f\n", EmExameFinal );
+				  System.out.println("Aluno aprovado.");
 			  }
 			else
 			  {
-			    System.out.printf("Aluno reprovado.\n");
-		//		System.out.printf("Media final: %.1f\n", EmExameFinal );
+			    System.out.println("Aluno reprovado");
 			  }
 		//	System.out.printf("Aluno reprovado.\n");	
-		  System.out.printf("Media final: %.1f\n", EmExameFinal );
+		  System.out.printf("Media final: %.1f%n", EmExameFinal );
 		} 
 		sc.close();
 	}
