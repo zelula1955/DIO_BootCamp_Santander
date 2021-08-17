fcd5eea3df2cdef588e1108a4a511c25391a5d21
Jose Luiz dos Santos <zelula1955@gmail.com>
2021-08-16 21:32:17 -0300

    Atualização dearquivo index.html

--- Projetos_DIO/personApi/src/main/java/com/digitalinnovation/personApi/controller/PersonController.java
+++ Projetos_DIO/personApi/src/main/java/com/digitalinnovation/personApi/controller/PersonController.java
@@ -1,17 +1,17 @@
 package com.digitalinnovation.personApi.controller;
 
 
-import com.digitalinnovation.personApi.service.PersonService;
+//import com.digitalinnovation.personApi.service.PersonService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/api/v1/people")
+@RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping("/")
-    public String helloMessage() {
+    @GetMapping
+    public String getBook() {
         return "Hello, Digital Innovation One!!";
     }
 }
