5480932087b89957ea4c1de429726876ec34263a
Jose Luiz dos Santos <zelula1955@gmail.com>
2021-08-29 18:06:37 -0300

    Criação de microserciços utilizando Spring Cloud - Product_Catalog

---
+++ Projetos_DIO/dio-experts-version-4/product-catalog/src/main/java/digitalinnovation/one/experts/productcatalog/ProductCatalogApplication.java
@@ -0,0 +1,14 @@
+package digitalinnovation.one.experts.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProductCatalogApplication.class, args);
+	}
+
+}
